0b0b62974e9f5e6b72dbe2d9fabcb3253c115311
tubaseven <34458605+tubaseven@users.noreply.github.com>
2021-12-18 14:14:27 +0300

    Add files via upload

---
+++ ShopiOrderingApp.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShopiOrderingApp.Application.Feature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopiOrderingApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = new GetAllOrdersQuery();
+            return Ok(await mediator.Send(query));
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetOrderByIdQuery() {   Id = id };
+            return Ok(await mediator.Send(query));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateOrdersCommand command)
+        {
+            return Ok(await mediator.Send(command));
+        }
+    }
+}
